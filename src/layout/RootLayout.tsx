cebcbd3dbf18bedc7a20ce8529e2449967b9d730
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-03-28 12:23:16 +0100

    fix: small layout adjustment

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -14,7 +14,7 @@ const RootLayout = ({ initialTheme, children }: RootLayoutProps) => {
         Jump directly to main content
       </a>
       <Navbar />
-      <main id="main" className="py-6 sm:py-20 lg:py-10">
+      <main id="main" className="sm:pt-25 pt-32 pb-20 lg:pt-10">
         {children}
       </main>
     </ThemeProvider>
