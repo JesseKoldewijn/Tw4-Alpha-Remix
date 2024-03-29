1c227c3195967376d4120412aff51374d887b85f
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-03-29 15:00:16 +0100

    fix: added vercel preset

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { vercelPreset } from "@vercel/remix/vite";
 import legacy from "@vitejs/plugin-legacy";
 import { type UserConfig, defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
@@ -13,6 +14,7 @@ import { webmanifest } from "./src/config/webmanifest";
 installGlobals();
 
 const remixConfig: VitePluginConfig = {
+  presets: [vercelPreset()],
   // Remix config
   appDirectory: "src/app",
 };
