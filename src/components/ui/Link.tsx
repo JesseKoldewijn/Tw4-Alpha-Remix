aafcb43148046736d967c2b64cd9e68213b9aeb6
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-03-29 11:05:24 +0100

    fix: small tweak on active state of link components

--- src/components/ui/Link.tsx
+++ src/components/ui/Link.tsx
@@ -31,21 +31,19 @@ export const LinkButton = forwardRef<
   }
 >(
   (
-    { srDescription, variant, className, href, showActive = true, ...rest },
+    {
+      srDescription,
+      variant,
+      className,
+      href,
+      showActive = true,
+      children,
+      "aria-current": ariaCurrent,
+      ...rest
+    },
     ref,
   ) => {
-    const loc = useLocation();
-
-    const matchHref = (href?: string) => {
-      if (!href) return false;
-      const hrefIsPathname = href.startsWith("/");
-
-      if (hrefIsPathname) {
-        return loc.pathname === href;
-      } else {
-        return loc.href === href;
-      }
-    };
+    if (!href) return <></>;
 
     return (
       <Button ref={ref} variant={variant ?? "link"} asChild>
@@ -53,13 +51,57 @@ export const LinkButton = forwardRef<
           href={href}
           className={cn(
             className,
-            showActive && matchHref(href) && "underline underline-offset-2",
+            showActive && MatchHref(href) && "underline underline-offset-2",
           )}
-          {...rest}
+          aria-current={
+            ariaCurrent ?? (showActive && MatchHref(href)) ? "page" : undefined
+          }
           srDescription={srDescription}
-        />
+          {...rest}
+        >
+          {children}
+          <ActiveLinkSrLabel href={href} />
+        </Link>
       </Button>
     );
   },
 );
 LinkButton.displayName = "LinkButtonComponent";
+
+/**
+ * The MatchHref function compares the current location pathname or href with a given href string to
+ * determine if they match.
+ * @param {string} href - The `href` parameter is a string that represents either a pathname or a full
+ * URL.
+ * @returns The function `MatchHref` is returning a boolean value. It checks if the `href` parameter is
+ * a pathname or a full URL and then compares it with the current location pathname or href. The
+ * function returns `true` if there is a match and `false` if there is no match or if the `href`
+ * parameter is empty.
+ */
+const MatchHref = (href: string) => {
+  const loc = useLocation();
+
+  if (!href) return false;
+  const hrefIsPathname = href.startsWith("/");
+
+  if (hrefIsPathname) {
+    return loc.pathname === href;
+  } else {
+    return loc.href === href;
+  }
+};
+
+/**
+ * The ActiveLinkSrLabel component renders a visually hidden message indicating that the current page
+ * is being displayed.
+ * @param  - The `ActiveLinkSrLabel` component takes a `href` prop of type string. It checks if the
+ * `href` matches a certain condition using the `MatchHref` function and if the `href` is not empty. If
+ * the conditions are met, it renders a `<span>` element with
+ * @returns A span element with the text "This is the current page" inside, with a class name of
+ * "sr-only".
+ */
+const ActiveLinkSrLabel = ({ href }: { href: string }) => {
+  if (!MatchHref(href) || !href) return <></>;
+
+  return <span className="sr-only">This is the current page</span>;
+};
